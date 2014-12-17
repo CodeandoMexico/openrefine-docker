f38a654808bc247f19b1752899a9a6ea5b117c78
Miguel Angel Gordian <os.aioria@gmail.com>
2014-12-17 17:15:57 -0600

    Get a fresh list of containers

--- sessions/open_refine.py
+++ sessions/open_refine.py
@@ -10,19 +10,17 @@ logger = logging.getLogger(__name__)
 
 SERVICE_NAME = "docker-openrefine"
 
-containers = ["http://" +
-              i.public_dns + ":" +
-              str(i.container_ports[0]["outer_port"])
-                for i in tutum.Container.list(state="Running")
-                if i.name == SERVICE_NAME ]
-
-
 # instances = Dict([zip(i, [False, False]) for i in containers])
 
 def get_session(ip):
     return False
 
 def new_session(ip):
+    containers = ["http://refinadora.datamx.io:" +
+              str(i.container_ports[0]["outer_port"])
+                for i in tutum.Container.list(state="Running")
+                if i.name == SERVICE_NAME ]
+
     instance = containers[int(random()  * 10)]
     logger.info("Instancia seleccionada: %s" % instance)
     return instance
