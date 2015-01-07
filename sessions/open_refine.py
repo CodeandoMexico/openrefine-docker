f99bd1fe35dcf5a35311135561ad7a61fd91cacd
Miguel Angel Gordian <os.aioria@gmail.com>
2015-01-07 16:11:37 -0600

    Supports diferents services names

--- sessions/open_refine.py
+++ sessions/open_refine.py
@@ -2,14 +2,15 @@
 import tutum
 from random import random
 import logging
+from os import getenv
 
 logger = logging.getLogger(__name__)
 
 # tutum.user = "miguel_angel"
 # tutum.apikey  = ""
 
-SERVICE_NAME = "docker-openrefine"
 
+SERVICE_NAME = getenv("TUTUM_SERVICE", "openrefine-geturl")
 # instances = Dict([zip(i, [False, False]) for i in containers])
 
 def get_session(ip):
@@ -19,8 +20,8 @@ def new_session(ip):
     containers = ["http://refinadora.datamx.io:" +
               str(i.container_ports[0]["outer_port"])
                 for i in tutum.Container.list(state="Running")
-                if i.name == SERVICE_NAME ]
+                if SERVICE_NAME in i.name]
 
-    instance = containers[int(random()  * 10)]
+    instance = containers[int(random()  * len(containers))]
     logger.info("Instancia seleccionada: %s" % instance)
     return instance
